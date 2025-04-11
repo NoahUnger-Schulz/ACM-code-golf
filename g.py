cf306e0f51318fe4ca025c6f54fc0504aa0785fd
Noah Unger-Schulz <noah@10-248-61-171.wireless.oregonstate.edu>
2025-04-10 20:04:43 -0700

    crazy python

--- g.py
+++ g.py
@@ -1,2 +1,2 @@
-import pandas as p
-print(p.Timestamp(input()).day_name())
+f=lambda a:["Tues","Wednes","Thurs","Fri","Satur","Sun","Mon"][(int(":400351362402"[a[1]])+a[2]+int(1.2425*a[0]))%7]
+print(f(list(map(int,input().split("-"))))+"day")
